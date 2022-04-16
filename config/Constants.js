807383217e1422ec3f86c89157fdf9994c84e3b4
Evi Owhotemu <evicoach@gmail.com>
2022-04-16 15:07:05 +0100

    Implemented queue listener and push to queue

---
+++ config/Constants.js
@@ -0,0 +1,15 @@
+module.exports = {
+    EMIT: {
+        PAYMENT_COMPLETED: "payment_completed"
+    },
+    QUEUE: {
+        TRANSACTION_QUEUE: "transaction_queue",
+        TRANSACTION_SUCCESS: "transaction_success",
+        TRANSACTION_FAILED: "transaction_failed"
+    },
+    STATUS: {
+        SUCCESS: "success",
+        PENDING: "pending",
+        FAILED: "failed"
+    }
+}
--- workers/BackgroundService.js
+++
@@ -1,4 +0,0 @@
-const axios = require("axios");
-const debug = require("debug");
-const RabbitMQ = require("amqp-library");
-
